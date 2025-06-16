216f1521b324649c74099782516b8ace754b2b30
Siddhartha2507 <siddharthadaram@gmail.com>
2025-06-16 19:08:07 +0530

    assignment7

---
+++ assignment_7.py
@@ -0,0 +1,50 @@
+#question1 
+def greet_user(name):
+    print(f"Hello, {name}!")
+
+greet_user("akshar")
+
+#question2
+def check_temperature(temp):
+ result = None
+ if temp > 99:
+    return "fever"
+ else:
+    return "normal"
+a = check_temperature(120)
+print(a) 
+
+#question3
+def get_last_fruits(a):
+   a = ["mango","orange","watermelon"]
+   return a[-1]
+b = get_last_fruits(a)
+print(b)
+
+#question4
+def get_price_tag(price):
+   result = None
+   if price > 1000:
+      return "expensive"
+   else:
+      return "affordable"
+   
+a = get_price_tag(999)
+print(a)
+
+#question5
+def format_user_info(name,age):
+   print(f"Name: {name},Age: {age}")
+
+a = format_user_info("akshar", 45)
+print(a)
+
+#question6
+def uppercase_if_string(a):
+   if
+   
+   
+
+ 
+ 
+
