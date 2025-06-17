4fd0e35150c6ae19bea8a7537ccb6bb73a1e8fbb
Siddhartha2507 <siddharthadaram@gmail.com>
2025-06-17 13:00:32 +0530

    assignment_7

--- assignment_7.py
+++ assignment_7.py
@@ -40,8 +40,50 @@ a = format_user_info("akshar", 45)
 print(a)
 
 #question6
-def uppercase_if_string(a):
-   if
+#def uppercase_if_string(a):
+   
+
+#question7
+def safe_divide(num,den):
+   result = None
+   if den != 0 :
+      return result
+   else:
+      return "cannot divide"
+   
+a = safe_divide(47,38)
+print(a)
+
+#question8
+def check_login(details):
+   if details.get('password'):
+      return "login succesful"
+   else:
+      return "login unsuccesful"
+   
+user_information = {
+   "username" : "akshar_123",
+   "password" : "damn123"
+}
+print(check_login(user_information))
+
+#question9
+def get_full_name(first,last):
+   return f"{first} {last}".title()
+print(get_full_name(first="siddu",last="daram"))
+
+#question10
+def get_discounted_price(price,is_member):
+   if is_member :
+      return price * 0.9
+   else:
+      return price
+   
+print(get_discounted_price(100,True))
+
+
+
+
    
    
 
