e5e6d267a13d76afa8e78fd6a2d5640a3bd580f7
Siddhartha2507 <siddharthadaram@gmail.com>
2025-06-19 19:12:17 +0530

    assignment_9

---
+++ assignment_9.py
@@ -0,0 +1,34 @@
+#question1
+for numbers in range(6):
+    print(numbers)
+
+#question2
+for numbers in range(10,20,2):
+ 
+ print(numbers)
+
+#question3
+# user = input("enter 3 names:")
+
+# a = list(user)
+
+# for use in user:
+#    print(f"i like {use}") 
+
+#question4
+a = [2,3,4]
+
+for i in a:
+   print(i**2)
+
+#question5
+a = [{
+   "name" : "alice",
+   "age" : 35
+},
+{
+   "name" : "bob",
+   "age" : 34
+}]
+for i in a:
+   print(f"{i["name"]}, {i["age"]}")
