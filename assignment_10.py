58232bd1e9777961ab494cf24288d42cea8120d2
Siddhartha2507 <siddharthadaram@gmail.com>
2025-07-08 23:29:07 +0530

    asign10

--- assignment_10.py
+++ assignment_10.py
@@ -55,10 +55,69 @@ def move_vowels_to_end(a):
 
 print(move_vowels_to_end('education'))
 
+#question5
+a = 'banana'
+frequency = {}
+for char in a:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+print(frequency)
+
+#question6
+vowels = 'aeiou'
+sentence = 'hello world'
+result = ''
+for char in sentence:
+    if char in vowels:
+        result += char.upper()
+    else:
+        result += char
+
+print(result)
+
+#question7
+digits = '12345'
+for char in digits:
+    if 1 < 2 < 3 < 4 < 5:
+        print('true')
+
+#question8
+line = 'This is a beautiful sunny day'
+words = line.split()
+count = 0
+for word in words:
+    if len(word) > 4 :
+        count += 1
+print(count)    
+
+#question11
+# sentence = 'open ai builds tools'
+# words = sentence.split()
+
+# for word in words:
+#     if True:
+#         words.reverse()
+#     else:
+#         pass
+
+# print(sentence)
+s = 'open ai builds tools'
+words = s.split()
+reversed_words = words[::-1]
+result = ''.join(reversed_words)
+print(result)
+
+#question12
+x = 'a1b2c3'
+result = x[::2]
+print(result)
+
+
 
         
-
-
 
 
 
